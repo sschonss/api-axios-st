d197cce9192a068b8b8c636a7808738bd73f1dec
Eliezer Boeira <eliezerbuczak2016@gmail.com>
2023-08-10 11:07:14 -0300

    criado try catch

--- busca.js
+++ busca.js
@@ -1,3 +1,13 @@
 const busca_usuario = () => {
-    alert("Buscando usuario");
+    const nome = document.getElementById('nome').value;
+    if(nome == ''){
+        alert('nome obrigatorio')
+    }
+
+    try{
+        const response = axios.get("http://192.168.1.26/api/cliente/nome/"+nome)
+        alert(response.data.message)
+    }catch(error){
+        alert(error.response.status)
+    }
 }
